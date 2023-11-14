77e8fc0a65151fd93a1e59228b6b8b616b08d8af
Ka Wai Wong <kw.wong0923@gmail.com>
2023-11-14 18:48:21 -0500

    update

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -13,9 +13,11 @@ router.get(
 router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
   // Authentication successful, redirect or send user info
   console.log("-----user-----");
-  console.log(req.user)
+  console.log(req.user);
   const userData = JSON.stringify(req.user);
-  const deepLinkUrl = `com.google-oauth-testing://profile?user=${userData}`;
+  // const deepLinkUrl = `com.google-oauth-testing://profile?user=${encodeURIComponent(userData)}`;
+  const deepLinkUrl = `google-oauth-testing://profile?user=${userData}`;
+
   res.redirect(deepLinkUrl);
 });
 
