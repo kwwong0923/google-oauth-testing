a5b609749c996ae40d509355b9c73302ba244fa4
Ka Wai Wong <kw.wong0923@gmail.com>
2023-11-14 20:39:50 -0500

    update

--- src/routes/test-route.js
+++ src/routes/test-route.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
 
 router.get("/message", (req, res) => {
-  res.json({message: "I go to school by bus"});
+  res.json({ message: "I go to school by bus" });
 });
 
 router.get("/", (req, res) => {
+  const { linkingUri } = req.params;
+  console.log(linkingUri);
   res.status(200).json({ message: "This is online" });
+  res.redirect(`${linkingUri}message=hello`);
 });
 
 module.exports = router;
